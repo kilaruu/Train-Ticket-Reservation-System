c21f68bdbfc47578ec46319a154270311033d3a5
OPQjuly23 <143674950+OPQjuly23@users.noreply.github.com>
2023-12-25 11:42:31 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,2 +1,22 @@
-
+node {
+     stage ('git clone'){
+      git 'https://github.com/OPQjuly23/Train-Ticket-Reservation-System.git'
+    }
+    stage('maven build'){
+            sh 'mvn clean install'
+    }
+    
+    stage('SonarQube Analysis') {
+ withSonarQubeEnv(credentialsId: 'sonarqube-Token') {
+    // some block
+                 sh'mvn  sonar:sonar'
+            }
+        }
+  stage ('deploy') {
+              sshagent(['tomcatcred']) {
+    // some block
+            sh 'scp -o strictHostkeyChecking=no target/*.war ubuntu@13.232.191.194:/opt/tomcat/apache-tomcat-9.0.68/webapps/'
+} 
+           }
+}
 
