45b7e6be434190b322da90e382525aa59cbae3a7
kilaruu <152464044+kilaruu@users.noreply.github.com>
2023-12-28 15:56:11 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,21 +1,15 @@
 node {
      stage ('git clone'){
-      git 'https://github.com/OPQjuly23/Train-Ticket-Reservation-System.git'
+      git 'https://github.com/kilaruu/Train-Ticket-Reservation-System.git'
     }
     stage('maven build'){
             sh 'mvn clean install'
     }
     
-    stage('SonarQube Analysis') {
- withSonarQubeEnv(credentialsId: 'sonarqube-Token') {
-    // some block
-                 sh'mvn  sonar:sonar'
-            }
-        }
   stage ('deploy') {
               sshagent(['tomcatcred']) {
     // some block
-            sh 'scp -o strictHostkeyChecking=no target/*.war ubuntu@13.232.191.194:/opt/tomcat/apache-tomcat-9.0.68/webapps/'
+            sh 'cp target/*.war /opt/tomcat/apache-tomcat-9.0.68/webapps/'
 } 
            }
 }
